26a1c4c6b9da76525b0d6d22869fc51980c928ff
Nicolas Fontan <nico.fontan1@gmail.com>
2020-10-06 20:04:56 -0400

    Finished functions for creating an engineer and intern. Created a function that will render the HTML page, it will create an output folder if there isn't one already. Fixed up the checks for all functions to avoid most user input errors. So you can't enter a word for the ID or a number for name, or leave them blank.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const { type } = require("os");
+const { parse } = require("path");
 
 const teamMembers = [];
 const idArray = [];
@@ -27,7 +27,7 @@ function mainMenu()
                 message: "What is your manager's name?",
                 validate: answer =>
                 {
-                    if (answer !== "")
+                    if (answer !== "" && typeof(answer) == "string" && !(answer <= 0) && !(answer >= 0))
                     {
                         return true;
                     }
@@ -40,7 +40,7 @@ function mainMenu()
                 message: "What is your manager's ID number?",
                 validate: answer =>
                 {
-                    if (answer !== "" && !isNaN(typeof(parseInt(answer))))
+                    if (answer !== "" && parseFloat(answer) > 0)
                     {
                         return true;
                     }
@@ -66,7 +66,7 @@ function mainMenu()
                 message: "What is your manager's office number?",
                 validate: answer =>
                 {
-                    if (answer !== "" && !isNaN(typeof(parseInt(answer))))
+                    if (answer !== "" && parseFloat(answer) > 0)
                     {
                         return true;
                     }
@@ -80,18 +80,212 @@ function mainMenu()
             teamMembers.push(manager);
             idArray.push(answer.managerId);
 
-            // createTeam(); (will uncomment when finished)
+            createTeamMember();
         });
     }
-    // createManager(); (will uncomment when finished)
+    createManager();
+
+    function createTeamMember()
+    {
+        inquirer.prompt
+        ([
+            {
+                type: "list",
+                name: "teamChoice",
+                message: "Do you want to add another team member?",
+                choices:
+                [
+                    "Engineer",
+                    "Intern",
+                    "Nope, my team's all done!"
+                ]
+            }
+        ]).then(answer =>
+        {
+            // Creating Manager
+            switch (answer.teamChoice)
+            {
+                case "Engineer":
+                    return createEngineer();
+
+                case "Intern":
+                    return createIntern();
+
+                default:
+                    return renderHTML();
+            }
+        });
+    }
+
+    function createEngineer()
+    {
+        inquirer.prompt
+        ([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is your engineer's name?",
+                validate: answer =>
+                {
+                    if (answer !== "" && typeof(answer) == "string" && !(answer <= 0) && !(answer >= 0))
+                    {
+                        return true;
+                    }
+                    return "Please enter a name";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is your engineer's ID number?",
+                validate: answer =>
+                {
+                    if (answer !== "" && parseFloat(answer) > 0 && !(idArray.includes(answer)))
+                    {
+                        return true;
+                    }
+                    else if (idArray.includes(answer))
+                    {
+                        return "This ID is taken, please give another";
+                    }
+                    return "Please enter an ID number";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your engineer's email address?",
+                validate: answer =>
+                {
+                    if (answer !== "" && answer.includes("@") && answer.includes(".com"))
+                    {
+                        return true;
+                    }
+                    return "Please enter an email address";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is your engineer's github username?",
+                validate: answer =>
+                {
+                    if (answer !== "" && typeof(answer) == "string" && !(answer <= 0) && !(answer >= 0))
+                    {
+                        return true;
+                    }
+                    return "Please enter a github username";
+                }
+            }
+        ]).then(answer =>
+        {
+            // Creating Manager
+            const engineer = new Engineer(answer.engineerName, answer.engineerId, answer.engineerEmail, answer.engineerGithub);
+            teamMembers.push(engineer);
+            idArray.push(answer.engineerId);
+
+            createTeamMember();
+        });
+    }
+
+    function createIntern()
+    {
+        inquirer.prompt
+        ([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is your intern's name?",
+                validate: answer =>
+                {
+                    if (answer !== "" && typeof(answer) == "string" && !(answer <= 0) && !(answer >= 0))
+                    {
+                        return true;
+                    }
+                    return "Please enter a name";
+                }
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "What is your intern's ID number?",
+                validate: answer =>
+                {
+                    if (answer !== "" && parseFloat(answer) > 0 && !(idArray.includes(answer)))
+                    {
+                        return true;
+                    }
+                    else if (idArray.includes(answer))
+                    {
+                        return "This ID is taken, please give another";
+                    }
+                    return "Please enter an ID number";
+                }
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is your intern's email address?",
+                validate: answer =>
+                {
+                    if (answer !== "" && answer.includes("@") && answer.includes(".com"))
+                    {
+                        return true;
+                    }
+                    return "Please enter an email address";
+                }
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What school is your intern attending?",
+                validate: answer =>
+                {
+                    if (answer !== "" && typeof(answer) == "string" && !(answer <= 0) && !(answer >= 0))
+                    {
+                        return true;
+                    }
+                    return "Please enter a school";
+                }
+            }
+        ]).then(answer =>
+        {
+            // Creating Manager
+            const intern = new Intern(answer.internName, answer.internId, answer.internEmail, answer.internSchool);
+            teamMembers.push(intern);
+            idArray.push(answer.internId);
+
+            createTeamMember();
+        });
+    }
+
+    function renderHTML()
+    {
+        if (!fs.existsSync("./output"))
+        {
+            fs.mkdirSync("./output");
+        }
+        fs.writeFile(outputPath, render(teamMembers), err =>
+        {
+            if (err)
+            {
+                throw err;
+            }
+            else
+            {
+                console.log("File was rendered");
+            }
+        });
+    }
 }
+
+mainMenu();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
